f065c2b5d90afec1397e6b23f2beaeaad55b25c8
Maria  Morris <mariasemailbox@gmail.com>
2018-05-03 03:22:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,10 +17,10 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.detect do | win_combination |
-    board_location1 = win_combination[0]
-    board_location2 = win_combination[1]
-    board_location3 = win_combination[2]
-  puts "This board combo is #{board_location1} and #{board_location2} and board_location3"
-    #board[0] == board[1] && board[1] == board[2] && board[0] != " "
+    location1 = win_combination[0]
+    location2 = win_combination[1]
+    location3 = win_combination[2]
+  #puts "This board combo is #{board_location1} and #{board_location2} and board_location3"
+    board[location1] == board[location2] && board[location2] == board[location3] && board[0] != " "
   end
 end
